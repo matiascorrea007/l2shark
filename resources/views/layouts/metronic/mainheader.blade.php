6097cd619651003baf462734dbcf92575c5d5497
matiascorrea007 <matiascorreautn@gmail.com>
2017-05-10 03:49:27 -0300

    fix template admin

--- resources/views/layouts/metronic/mainheader.blade.php
+++ resources/views/layouts/metronic/mainheader.blade.php
@@ -1,145 +1,23 @@
  <!-- Efecto Parallax -->
-<div class="bg" style="background: url('images/banner.jpg') no-repeat center center;"></div>
+<div class="bg" style="background: url('images/banner2.jpg') no-repeat center center;"></div>
 <div class="jumbotron affix">
 </div>
 <!-- End Efecto Parallax -->
 
 
-<div class="page-header" id="main">
+<div class="page-header " id="main">
                         <!-- BEGIN HEADER TOP -->
-                        <div class="page-header-top">
+                        <div class="">
                             <div class="container-fluid">
                                 <!-- BEGIN LOGO -->
-                                <div class="page-logo">
-                                    <a href="index.html">
-                                        <img src="../assets/layouts/layout3/img/logo-default.jpg" alt="logo" class="logo-default">
-                                    </a>
-                                </div>
+                                
                                 <!-- END LOGO -->
                                 <!-- BEGIN RESPONSIVE MENU TOGGLER -->
                                 <a href="javascript:;" class="menu-toggler"></a>
                                 <!-- END RESPONSIVE MENU TOGGLER -->
                                 <!-- BEGIN TOP NAVIGATION MENU -->
                                 <div class="top-menu">
-                                    <ul class="nav navbar-nav pull-right">
-                                        <!-- BEGIN NOTIFICATION DROPDOWN -->
-                                        <!-- DOC: Apply "dropdown-hoverable" class after "dropdown" and remove data-toggle="dropdown" data-hover="dropdown" data-close-others="true" attributes to enable hover dropdown mode -->
-                                        <!-- DOC: Remove "dropdown-hoverable" and add data-toggle="dropdown" data-hover="dropdown" data-close-others="true" attributes to the below A element with dropdown-toggle class -->
-                                        <li class="dropdown dropdown-extended dropdown-notification dropdown-dark" id="header_notification_bar">
-                                            <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true" aria-expanded="false">
-                                                <i class="icon-bell"></i>
-                                                <span class="badge badge-default">7</span>
-                                            </a>
-                                            <ul class="dropdown-menu">
-                                                <li class="external">
-                                                    <h3>You have
-                                                        <strong>12 pending</strong> tasks</h3>
-                                                    <a href="app_todo.html">view all</a>
-                                                </li>
-                                                <li>
-                                                    <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 250px;"><ul class="dropdown-menu-list scroller" style="height: 250px; overflow: hidden; width: auto;" data-handle-color="#637283" data-initialized="1">
-
-                                                        <li>
-                                                            <a href="javascript:;">
-                                                                <span class="time">9 days</span>
-                                                                <span class="details">
-                                                                    <span class="label label-sm label-icon label-danger">
-                                                                        <i class="fa fa-bolt"></i>
-                                                                    </span> Storage server failed. </span>
-                                                            </a>
-                                                        </li>
-                                                    </ul><div class="slimScrollBar" style="background: rgb(99, 114, 131); width: 7px; position: absolute; top: 4px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 121.359px;"></div><div class="slimScrollRail" style="width: 7px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(234, 234, 234); opacity: 0.2; z-index: 90; right: 1px;"></div></div>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <!-- END NOTIFICATION DROPDOWN -->
-                                        <!-- BEGIN TODO DROPDOWN -->
-                                        <li class="dropdown dropdown-extended dropdown-tasks dropdown-dark" id="header_task_bar">
-                                            <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true" aria-expanded="false">
-                                                <i class="icon-calendar"></i>
-                                                <span class="badge badge-default">3</span>
-                                            </a>
-                                            <ul class="dropdown-menu extended tasks">
-                                                <li class="external">
-                                                    <h3>You have
-                                                        <strong>12 pending</strong> tasks</h3>
-                                                    <a href="app_todo_2.html">view all</a>
-                                                </li>
-                                                <li>
-                                                    <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 275px;"><ul class="dropdown-menu-list scroller" style="height: 275px; overflow: hidden; width: auto;" data-handle-color="#637283" data-initialized="1">
-                                                        <li>
-                                                            <a href="javascript:;">
-                                                                <span class="task">
-                                                                    <span class="desc">New release v1.2 </span>
-                                                                    <span class="percent">30%</span>
-                                                                </span>
-                                                                <span class="progress">
-                                                                    <span style="width: 40%;" class="progress-bar progress-bar-success" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100">
-                                                                        <span class="sr-only">40% Complete</span>
-                                                                    </span>
-                                                                </span>
-                                                            </a>
-                                                        </li>
-                                                        
-                                                    </ul><div class="slimScrollBar" style="background: rgb(99, 114, 131); width: 7px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 147.994px;"></div><div class="slimScrollRail" style="width: 7px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(234, 234, 234); opacity: 0.2; z-index: 90; right: 1px;"></div></div>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <!-- END TODO DROPDOWN -->
-                                        <li class="droddown dropdown-separator">
-                                            <span class="separator"></span>
-                                        </li>
-                                        <!-- BEGIN INBOX DROPDOWN -->
-                                        
-                                        <!-- END INBOX DROPDOWN -->
-
-
-                                        <!-- BEGIN USER LOGIN DROPDOWN -->
-                                        <li class="dropdown dropdown-user dropdown-dark">
-                                            <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-                                                <img alt="" class="img-circle" src="../assets/layouts/layout3/img/avatar9.jpg">
-                                                <span class="username username-hide-mobile">Nick</span>
-                                            </a>
-                                            <ul class="dropdown-menu dropdown-menu-default">
-                                                <li>
-                                                    <a href="page_user_profile_1.html">
-                                                        <i class="icon-user"></i> My Profile </a>
-                                                </li>
-                                                <li>
-                                                    <a href="app_calendar.html">
-                                                        <i class="icon-calendar"></i> My Calendar </a>
-                                                </li>
-                                                <li>
-                                                    <a href="app_inbox.html">
-                                                        <i class="icon-envelope-open"></i> My Inbox
-                                                        <span class="badge badge-danger"> 3 </span>
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <a href="app_todo_2.html">
-                                                        <i class="icon-rocket"></i> My Tasks
-                                                        <span class="badge badge-success"> 7 </span>
-                                                    </a>
-                                                </li>
-                                                <li class="divider"> </li>
-                                                <li>
-                                                    <a href="page_user_lock_1.html">
-                                                        <i class="icon-lock"></i> Lock Screen </a>
-                                                </li>
-                                                <li>
-                                                    <a href="page_user_login_1.html">
-                                                        <i class="icon-key"></i> Log Out </a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <!-- END USER LOGIN DROPDOWN -->
-                                        <!-- BEGIN QUICK SIDEBAR TOGGLER -->
-                                        <li class="dropdown dropdown-extended quick-sidebar-toggler">
-                                            <span class="sr-only">Toggle Quick Sidebar</span>
-                                            <i class="icon-logout"></i>
-                                        </li>
-                                        <!-- END QUICK SIDEBAR TOGGLER -->
-                                    </ul>
+                                    
                                 </div>
                                 <!-- END TOP NAVIGATION MENU -->
                             </div>
